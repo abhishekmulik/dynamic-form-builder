614ab8cc06bf5bb24b6e44823cab294f61e690d8
Abhishek <mulikabhishek521@gmail.com>
2025-08-16 19:21:41 +0530

    removed unused code from textarea

--- src/components/ui/atoms/Textarea.tsx
+++ src/components/ui/atoms/Textarea.tsx
@@ -1,19 +1,8 @@
 import { Textarea, TextareaProps } from "@chakra-ui/react";
-import { useEffect, useRef } from "react";
 
 export function TextArea(props: TextareaProps) {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-  }, [props.value]);
-
   return (
     <Textarea
-      ref={textareaRef}
       placeholder="Comment..."
       _hover={{
         borderColor: "none"
